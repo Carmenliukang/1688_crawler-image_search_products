a672e89c1349a5a1ce93eded62992f4256e919a4
刘康 <liukang.hero@bytedance.com>
2020-07-08 14:33:40 +0800

    delete taobao lib

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from lib.taobao_lib import TaoBao
 from lib.alibaba_lib import Alibaba
 
 if __name__ == '__main__':
@@ -10,5 +9,3 @@ if __name__ == '__main__':
     url = Alibaba().run(filename)
     print(url)
 
-    # url = TaoBao().run(filename)
-    # print(url)
