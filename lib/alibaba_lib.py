25dc620d42287cf674c72dcd0a9da5148bfac348
liukang <17621173188@163.com>
2021-02-23 17:43:47 +0800

    适配最新版本

--- lib/alibaba_lib.py
+++ lib/alibaba_lib.py
@@ -3,9 +3,12 @@
 
 import time
 import base64
+import json
 from lib.func_txy import request_get
 from lib.func_txy import request_post
+from lib.func_txy import request_get_content
 from lib.func_txy import get_random_str
+from lib.func_txy import get_random_digits
 import os.path
 from urllib.parse import urlparse
 import requests
@@ -17,82 +20,32 @@ class Alibaba(object):
     1688 PC 端接口获取相似商品的接口
     """
 
-    def __init__(self):
-        self.upload_url = 'https://cbusearch.oss-cn-shanghai.aliyuncs.com/'  # 上传图片
-        self.sign_url = "https://open-s.1688.com/openservice/ossDataService"  # 获取 sign 加密
+    def __init__(self, cookie):
+        self.upload_url = "https://stream-upload.taobao.com/api/upload.api?appkey=1688search&folderId=0&_input_charset=utf-8&useGtrSessionFilter=false"  # 上传图片
         self.imageSearch_service_url = "https://open-s.1688.com/openservice/imageSearchOfferResultViewService"
-        self._headers()
+        self._headers(cookie=cookie)
         self.search_page_size = 40
 
     def setSearchPageSize(self, pageSize):
         self.search_page_size = pageSize
 
-    def _headers(self):
+    def _headers(self, cookie):
         headres = {
             'Origin': "https://www.1688.com",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:85.0) Gecko/20100101 Firefox/85.0",
             "Accept": "*/*",
             "Cache-Control": "no-cache",
-            "refer": "https://www.1688.com/"
+            "refer": "https://www.1688.com/",
+            "cookie": cookie
         }
         self.headers = headres
 
-    def get_key(self):
-        '''
-            cbuimgsearch/Z5Qja4fXPH1562293605000.jpg 生成规则是 cbuimgsearch/ + 随机10位 + 毫秒级别时间戳
-            :return:
-            '''
-        key = "cbuimgsearch/" + "".join(get_random_str(10)) + str(int(time.time() * 1000))
-        return key
-
-    def get_dateset(self):
-        '''
-        获取加密时间
-        :return:
-        '''
-        url = "https://open-s.1688.com/openservice/.htm?"  # 获取加密时间
-        params = {
-            "serviceIds": "cbu.searchweb.config.system.currenttime",
-            "outfmt": "json",
-        }
-
-        status, data = request_get(url, params, self.headers)
-        return status, data
-
-    def get_sign(self, data_set):
-        '''
-        用于获取 sign 用于加密
-        :return:
-        '''
-        url = 'https://open-s.1688.com/openservice/ossDataService'
-        key = str(base64.b64decode("cGNfdHVzb3U=".encode("utf-8")), encoding="utf-8")
-        appkey = "{};{}".format(key, str(data_set))
-
-        params = {
-            "appName": key,
-            "appKey": base64.b64encode(appkey.encode("utf-8")),
-        }
-
-        status, data = request_get(url, params, self.headers)
-
-        data = data.get('data', {})
-
-        signature = data.get('signature', '')
-        policy = data.get('policy', '')
-        accessid = data.get('accessid', '')
-
-        return status, signature, policy, accessid
-
-    def upload_img(self, filename, signature, policy, accessid):
+    def upload_img(self, filename):
         """
         用于上传图片
         :return:
         """
-
-        url = 'https://cbusearch.oss-cn-shanghai.aliyuncs.com/'
-        key = "cbuimgsearch/" + get_random_str(10) + str(int(time.time()) * 1000) + ".jepg"
         name = get_random_str(5) + ".jpeg"
-
         if os.path.exists(filename):
             bytestream = open(filename, "rb").read()
         else:
@@ -104,20 +57,21 @@ class Alibaba(object):
 
         files = {
             "name": (None, name),
-            "key": (None, key),
-            "policy": (None, policy),
-            "OSSAccessKeyId": (None, accessid),
-            "success_action_status": (None, 200),
-            "callback": (None, ""),
-            "signature": (None, signature),
+            # "ua": (None, ""),
             "file": (name, bytestream)
         }
 
-        status, res = request_post(url, data=None, files=files, headers=self.headers)
+        status, res = request_post(self.upload_url, data=None, files=files, headers=self.headers)
+        key = ""
+        if status == "succ":
+            data = json.loads(res)
+            url = data["object"]["url"]
+            key = url.split("/")[-1]
         return status, key
 
     def img_search(self, img_key, dataSet, beginPage=1):
         """
+        todo 暂未升级到最新版本
         用于上传图片并搜索商品列表
         从1688官网图搜页面扒出来的jsonp接口
         :return: dict o None
@@ -145,24 +99,16 @@ class Alibaba(object):
             return 'fail', None
 
     def run(self, filename, need_products=False):
-        status, data = self.get_dateset()
-
-        # json 直接解析
-        data_set = data.get('cbu.searchweb.config.system.currenttime', {}).get('dataSet', '')
-
-        # 获取相关的 接口验证参数。同时 获取生成的 图片key
-        status, signature, policy, accessid = self.get_sign(data_set)
-
         # uoload image file
-        status, key = self.upload_img(filename, signature, policy, accessid)
+        status, key = self.upload_img(filename)
+
         # 上传成功后，拼接生成的 查询 URL
         if status == "succ":
-            url_res = 'https://s.1688.com/youyuan/index.htm?tab=imageSearch&imageType=oss&imageAddress={}&spm='.format(
-                key)
+            url_res = f"https://s.1688.com/youyuan/index.htm?tab=imageSearch&imageAddress={key}&spm="
             if need_products == False:
                 return url_res
             else:
-                status_desc, data = self.img_search(key, data_set)
+                status_desc, data = self.img_search(key, "")
                 if status_desc == 'succ':
                     return data
                 return None
--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from lib.alibaba_lib import Alibaba
 
 if __name__ == '__main__':
     filename = 'data/下载.jpeg'
-
-    url = Alibaba().run(filename)
+    cookie = """请填写登入成功的cookie"""
+    url = Alibaba(cookie).run(filename)
     print(url)
-
