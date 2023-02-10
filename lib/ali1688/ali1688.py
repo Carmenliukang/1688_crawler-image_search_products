c7b49cb164f0014a64b92a43221b7bf890e9c1df
fenghanye <31881008+Carmenliukang@users.noreply.github.com>
2023-02-10 23:06:27 +0800

    added taobao api

--- lib/ali1688/ali1688.py
+++ lib/ali1688/ali1688.py
@@ -21,24 +21,25 @@ class Ali1688(object):
 
     def headers(self):
         headres = {
-            "Origin": "https://s.1688.com",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:85.0) Gecko/20100101 Firefox/85.0",
-            "referer": "https://s.1688.com",
         }
         return headres
 
 
 class Token(Ali1688):
-    def __init__(self):
-        self.url = "https://h5api.m.1688.com/h5/mtop.taobao.widgetservice.getjsoncomponent/1.0/?jsv=2.7.0&appKey=12574478&t=1662222785423&api=mtop.taobao.widgetService.getJsonComponent&v=1.0&type=json&dataType=jsonp&callback=mtopjsonp1"
+    def __init__(self, api: str, hostname: str):
+        self.api = api
+        self.hostname = hostname
+        self.token_url = f"https://{self.hostname}/h5/{self.api.lower()}/1.0/"
+        self.cookies: RequestsCookieJar
         super(Token, self).__init__()
 
-    def get_params(self) -> Dict[str, str]:
+    def get_token_params(self) -> Dict[str, str]:
         params = {
             "jsv": "2.7.0",
             "appKey": self.app_key,
             "t": str(self.t),
-            "api": "mtop.taobao.widgetService.getJsonComponent",
+            "api": self.api,
             "v": "1.0",
             "type": "json",
             "dataType": "jsonp",
@@ -47,18 +48,11 @@ class Token(Ali1688):
         return params
 
     def request(self) -> requests.request:
-        params = self.get_params()
+        params = self.get_token_params()
         headers = self.headers()
-        req = request_get(url=self.url, params=params, headers=headers)
+        req = request_get(url=self.token_url, params=params, headers=headers)
+        self.cookies: RequestsCookieJar = req.cookies
         return req
-
-
-class Upload(Ali1688):
-    def __init__(self, cookies: RequestsCookieJar):
-        self.cookies: RequestsCookieJar = cookies
-        self.url = "https://h5api.m.1688.com/h5/mtop.1688.imageservice.putimage/1.0"
-        self._get_token()
-        super(Upload, self).__init__()
 
     def _get_token(self):
         if not self.cookies or not self.cookies.get("_m_h5_tk", ""):
@@ -76,13 +70,21 @@ class Upload(Ali1688):
         sign_str = sign.sign(text)
         return sign_str
 
-    def get_params(self, data: str, t: int) -> Dict[str, str]:
+
+class Ali1688Upload(Token):
+    def __init__(self, api: str = "mtop.1688.imageService.putImage", hostname="h5api.m.taobao.com"):
+        super(Ali1688Upload, self).__init__(api=api, hostname=hostname)
+        self.upload_url = f"https://{self.hostname}/h5/{self.api.lower()}/1.0"
+        self.request()
+        self._get_token()
+
+    def get_params(self, data: str, t: int, jsv: str = "2.4.11") -> Dict[str, str]:
         sign_str = self.get_sign(data=data, t=t)
         params = {
-            "jsv": "2.4.11",
+            "jsv": jsv,
             "appKey": self.app_key,
             "t": str(t),
-            "api": "mtop.1688.imageService.putImage",
+            "api": self.api,
             "ecode": "0",
             "v": "1.0",
             "type": "originaljson",
@@ -113,7 +115,7 @@ class Upload(Ali1688):
         headers = self.headers()
         headers["Content-Type"] = "application/x-www-form-urlencoded"
         req = request_post(
-            url=self.url,
+            url=self.upload_url,
             params=params,
             headers=headers,
             data=data,
@@ -122,10 +124,36 @@ class Upload(Ali1688):
         return req
 
 
-class ImageSearch(Ali1688):
+class WorldTaobao(Ali1688Upload):
+    def __init__(self, api: str = "mtop.tmall.hk.yx.worldhomepagepcapi.gethotwords", hostname="h5api.m.taobao.com"):
+        super(WorldTaobao, self).__init__(api=api, hostname=hostname)
+        self.upload_url = f"https://{self.hostname}/h5/mtop.relationrecommend.wirelessrecommend.recommend/2.0/"
+
+    def get_data(self, filename: str) -> Dict[str, str]:
+        # get file bytes
+        with open(filename, "rb") as f:
+            b64_bytes = base64.b64encode(f.read())
+        strimg = str(b64_bytes).replace("b'", "").replace("'", "").replace("==", "")
+        params = json.dumps(
+            {
+                "strimg": strimg,
+                "pcGraphSearch": True,
+                "sortOrder": 0,
+                "tab": "all",
+                "vm": "nv",
+            },
+            separators=(",", ":"),
+        )
+
+        data = json.dumps({"params": params, "appId": "34850"}, separators=(",", ":"), )
+
+        return {"data": data}
+
+
+class Ali1688ImageSearch(Ali1688):
     def __init__(self):
         self.url = "https://s.1688.com/youyuan/index.htm"
-        super(ImageSearch, self).__init__()
+        super(Ali1688ImageSearch, self).__init__()
 
     def get_params(self, image_id: str) -> Dict[str, str]:
         params = {"tab": "imageSearch", "imageId": image_id, "imageIdList": image_id}
